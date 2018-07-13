431e589497881918203c9a701cf966b9f3d710d2
Fuego <fuegozero35@gmail.com>
2018-07-13 18:14:51 +0800

    update

--- src/project-config/WeChatApplet-base.js
+++ src/project-config/WeChatApplet-base.js
@@ -1,7 +1,12 @@
 module.exports = {
-    exclude: {
-        file     : ['jsconfig.json', 'project.config.json'],
-        extname  : ['.less'],
-        directory: ['.svn', '.vscode', 'typings']
-    }
-};
+  exclude: {
+    file     : [
+      'jsconfig.json',
+      'project.config.json',
+      'package.json',
+      'package-lock.json',
+    ],
+    extname  : ['.less'],
+    directory: ['.svn', '.vscode', 'typings', 'node_modules'],
+  },
+}
